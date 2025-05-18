8b2369da263a10a2fde09ccd4b929bf24a0fdb9b
volochayev <stingray8888831@gmail.com>
2025-05-18 09:13:45 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const elements = event.target.elements;
+
+    const emailValue = elements.email.value.trim();
+    const passwordValue = elements.password.value.trim();
+
+    if (emailValue === "" || passwordValue === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+
+    const formInfo = {
+        email: emailValue,
+        password: passwordValue
+    };
+
+    console.log(formInfo);
+
+    event.target.reset();
+    
+});
+
+
 
